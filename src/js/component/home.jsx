bb038a2f47e3f1e08972c8f03f9d085071d509ef
Diego Vargas <105869790+diego0o033@users.noreply.github.com>
2024-07-12 02:18:03 +0000

    proyecto finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import Counter from "./counter";
 
 //create your first component
-// cuando paso las props, los debo colocar en un solo objeto!
+// cuando paso las props, los debo colocar entre llaves!
 const Home = ({contador_1,contador_2,contador_3,contador_4,contador_5,contador_6}) => {
 	return (
-		// genero el contenedor principal para almacenar cada uno de los contenedores
+		// genero el contenedor principal para almacenar cada uno de los componentes "Counter"
 		<div className="container-fluid d-flex justify-content-between gap-3 bg-black p-3">
-				{/* inserto cada uno de los componentes que van a contener los numeros */}
+				{/* inserto cada uno de los componentes que van a contener los contadores */}
 				<Counter elemento_contador={"🕑"}/>
 				<Counter elemento_contador={contador_6}/>
 				<Counter elemento_contador={contador_5}/>
--- src/js/index.js
+++ src/js/index.js
@@ -7,38 +7,40 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+//declaro las variables que me van  a permitir aumentar mis contadores
+let contador_1 = 0;
+let contador_2 = 0;
+let contador_3 = 0;
+let contador_4 = 0;
+let contador_5 = 0;
+let contador_6 = 0;
+//utilizo la funcion setInterval() para aumentar los contadores dentro de un intervalo de tiempo especifico
+setInterval(Counter_React, 1000);
+//genero la funcion que me va a permitir aumentar los contadores
+function Counter_React() {
 
-let contador_1=0;
-let contador_2=0;
-let contador_3=0;
-let contador_4=0;
-let contador_5=0;
-let contador_6=0;
+    contador_1++;
+    if (contador_1 > 9) {
+        contador_2++;
+        contador_1 = 0;
 
-setInterval(Counter_React,1000);
+    } else if (contador_2 > 9) {
+        contador_3++;
+        contador_2 = 0;
+    } else if (contador_3 > 9) {
+        contador_4++;
+        contador_3 = 0;
 
-function Counter_React(){
-    
-            contador_1++;
-        if(contador_1>9){
-            contador_2++;
-            contador_1=0;
-        
-        }else if(contador_2>9){
-            contador_3++;
-            contador_2=0;
-        }else if(contador_3>9){
-            contador_4++;
-            contador_3=0;
-    
-        }else if(contador_4>9){
-            contador_5++;
-            contador_4=0;
-    
-        }else if(contador_5>9){
-            contador_6++;
-            contador_5=0;
-        }
-        //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home contador_1={contador_1} contador_2={contador_2} contador_3={contador_3} contador_4={contador_4} contador_5={contador_5} contador_6={contador_6}/>)}
+    } else if (contador_4 > 9) {
+        contador_5++;
+        contador_4 = 0;
+
+    } else if (contador_5 > 9) {
+        contador_6++;
+        contador_5 = 0;
+    }
+    //renderizo mi componente de React
+    ReactDOM.createRoot(document.getElementById('app')).render(<Home contador_1={contador_1} contador_2={contador_2} 
+        contador_3={contador_3} contador_4={contador_4} contador_5={contador_5} contador_6={contador_6} />)
+}
 
